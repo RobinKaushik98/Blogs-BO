9ba23598c43b8118fbf566808f1c573c42a89dba
RobinKaushik98 <robinkaushik315@gmail.com>
2024-03-10 14:47:45 +0530

    first commit

---
+++ src/main/java/com/files/bo/RobinKaushikBlogsBoApplication.java
@@ -0,0 +1,13 @@
+package com.files.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinKaushikBlogsBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinKaushikBlogsBoApplication.class, args);
+	}
+
+}
